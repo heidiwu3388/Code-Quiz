9d8ec462e4d2580c768775ccf7434b85ac74a4e7
heidiwu3388 <heidiwu3388@yahoo.com>
2022-10-20 22:31:31 -0700

    add function for presenting quiz question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,76 @@
-var timeLeft = 0;
-var quizEnd = true;
-var intervalId;
-var startEl = document.querySelector("#btn-start");
-var cardStartEl = document.querySelector("#card-start");
-var cardQuizEl = document.querySelector("#card-quiz");
-var timeEl = document.querySelector("#time");
+let timeLeft = 0;
+let quizEnd = true;
+let intervalId;
+let currentQuestion = 0; //index of the current question
+const startEl = document.querySelector("#btn-start");
+const cardStartEl = document.querySelector("#card-start");
+const cardQuizEl = document.querySelector("#card-quiz");
+const timeEl = document.querySelector("#time");
+const questionEl = document.querySelector("#question");
+const choicesEl = document.querySelector("#choices");
+const quiz = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?", 
+        choices: ["<script>",
+                "<scripting>",
+                "<js>",
+                "<javascript>"],
+        answerindex: 0
+    },
+    {
+        question: "How to write an IF statement in JavaScript?", 
+        choices: ["if i == 5 then",
+                "if i = 5",
+                "if ( i == 5 )",
+                "if i = 5 then"],
+        answerindex: 2
+    },
+    {
+        question: "Which is the correct way to write a JavaScript array", 
+        choices: ["var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+                "var colors = 'red', 'green', 'blue'",
+                "var colors = (1:'red', 2:'green', 3:'blue')",
+                "var colors = ['red', 'green', 'blue']"],
+        answerindex: 3
+    },
+    {
+        question: "Who is making the Web standards?", 
+        choices: ["The World Wide Web COnsortium",
+                "Microsoft",
+                "Google",
+                "Mozilla"],
+        answerindex: 0
+    },
+    {
+        question: "How can you open a link in a new tab/browser window?", 
+        choices: ["&la href='url' target='_bland'>",
+                "&la href='url' target='new'>",
+                "&la href='url' new>",
+                "&la href='url' _blank'>"],
+        answerindex: 0
+    },
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?", 
+        choices: ["required",
+                "placeholder",
+                "validate",
+                "formvalidate"],
+        answerindex: 0
+    }
+];
+
+function presentQuestion() {
+    // check if there is still question
+    if (currentQuestion >= quiz.length) return;
+
+    // present question
+    questionEl.textContent = quiz[currentQuestion].question;
+    // present the 4 choices and set up the attribute data-answer
+    for (i=0; i<4; i++) {
+        choicesEl.children[i].textContent = quiz[currentQuestion].choices[i];
+        choicesEl.children[i].setAttribute("data-answer", i===quiz[currentQuestion].answerindex);
+    }
+};
 
 // when Start button is clicked
 startEl.addEventListener("click", function() {
@@ -25,5 +91,5 @@ startEl.addEventListener("click", function() {
         }
     },1000);
     // present the question one by one
-    
+    presentQuestion();
 });
