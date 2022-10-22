427336b8369eac416914f39c0097ea75139d8e53
heidiwu3388 <heidiwu3388@yahoo.com>
2022-10-21 20:47:31 -0700

    add code to retrieve data from local storage and display in HighScores page

---
+++ assets/js/highscores.js
@@ -0,0 +1,29 @@
+const listEl = document.querySelector("#highscores-list");
+const goBackEl = document.querySelector("#go-back");
+const clearEl = document.querySelector("#clear");
+
+// get all high scores form local storage
+let allHighscores = JSON.parse(localStorage.getItem("highscores") || "[]");
+
+// display each highscore in a dynamically created <li> 
+allHighscores.forEach(element => {
+    console.log(element.initials, element.score);
+    let highscoreEl = document.createElement("div");
+    highscoreEl.textContent = element.initials + " - " + element.score;
+    listEl.appendChild(highscoreEl);
+});
+
+// when GO BACK button is clicked, go back to main page (index.html)
+goBackEl.addEventListener("click", function(){
+    console.log("go back");
+    location.href = "./index.html"
+});
+
+// when CLEAR HIGH SCORES button is clicked
+clearEl.addEventListener("click", function() {
+    // remove "highscore" from local storage
+    localStorage.removeItem("highscores");
+    // clear display
+    listEl.innerHTML="";
+});
+
